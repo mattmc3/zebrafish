9f85067bb8a6e51c0fee59a1347bd0d9a36f0fd8
mattmc3 <mattmc3@gmail.com>
2019-11-03 09:36:43 -0500

    Enhance compinit feature and add zcompile

--- zebrafish.zsh
+++ zebrafish.zsh
@@ -210,18 +210,28 @@ if ! (($_disabled_features[(Ie)key-bindings])); then
   bindkey -s '^[[Z' '\t'
 fi
 
-
-### Completions
-if ! (($_disabled_features[(Ie)compinit])); then
+#
+## Completions
+_zcompinit() {
   # https://github.com/sorin-ionescu/prezto/blob/master/modules/completion/init.zsh#L31-L41
+  # http://zsh.sourceforge.net/Doc/Release/Completion-System.html#Use-of-compinit
+  # https://gist.github.com/ctechols/ca1035271ad134841284#gistcomment-2894219
+  setopt extendedglob local_options
   autoload -Uz compinit
-  _comp_files=(${ZDOTDIR:-$HOME}/.zcompdump(Nm-20))
-  if (( $#_comp_files )); then
-    compinit -i -C
+  local zcd=${ZDOTDIR:-$HOME}/.zcompdump
+  local zcdc="$zcd.zwc"
+  # Compile the completion dump to increase startup speed if dump is newer or
+  # missing. Do in background for next time to not affect the current session
+  if [[ -f "$zcd"(#qN.m+1) ]]; then
+    compinit -i -d "$zcd"
+    { rm -f "$zcdc" && zcompile "$zcd" } &!
   else
-    compinit -i
+    compinit -i -C -d "$zcd"
+    { [[ ! -f "$zcdc" || "$zcd" -nt "$zcdc" ]] && rm -f "$zcdc" && zcompile "$zcd" } &!
   fi
-  unset _comp_files
+}
+if ! (($_disabled_features[(Ie)compinit])); then
+  _zcompinit
 fi
 
 
