079021048195363f1388fbfe8cee453b7b131af2
mattmc3 <mattmc3@gmail.com>
2019-11-03 20:19:32 -0500

    Move history options to history feature

--- zebrafish.plugin.zsh
+++ zebrafish.plugin.zsh
@@ -59,20 +59,7 @@ setopt extended_glob           # use more awesome globbing features
 setopt glob_dots               # include dotfiles when globbing
 
 # History
-setopt append_history          # append to history file
-setopt extended_history        # write the history file in the ':start:elapsed;command' format
-unsetopt hist_beep             # don't beep when attempting to access a missing history entry
-setopt hist_expire_dups_first  # expire a duplicate event first when trimming history
-setopt hist_find_no_dups       # don't display a previously found event
-setopt hist_ignore_all_dups    # delete an old recorded event if a new event is a duplicate
-setopt hist_ignore_dups        # don't record an event that was just recorded again
-setopt hist_ignore_space       # don't record an event starting with a space
-setopt hist_no_store           # don't store history commands
-setopt hist_reduce_blanks      # remove superfluous blanks from each command line being added to the history list
-setopt hist_save_no_dups       # don't write a duplicate event to the history file
-setopt hist_verify             # don't execute immediately upon history expansion
-setopt inc_append_history      # write to the history file immediately, not when the shell exits
-unsetopt share_history         # don't share history between all sessions
+# set in history feature
 
 # Initialization
 # none
@@ -131,6 +118,22 @@ fi
 #
 ## History
 if ! (($_disabled_features[(Ie)history])); then
+  # http://zsh.sourceforge.net/Doc/Release/Options.html#History
+  setopt append_history          # append to history file
+  setopt extended_history        # write the history file in the ':start:elapsed;command' format
+  unsetopt hist_beep             # don't beep when attempting to access a missing history entry
+  setopt hist_expire_dups_first  # expire a duplicate event first when trimming history
+  setopt hist_find_no_dups       # don't display a previously found event
+  setopt hist_ignore_all_dups    # delete an old recorded event if a new event is a duplicate
+  setopt hist_ignore_dups        # don't record an event that was just recorded again
+  setopt hist_ignore_space       # don't record an event starting with a space
+  setopt hist_no_store           # don't store history commands
+  setopt hist_reduce_blanks      # remove superfluous blanks from each command line being added to the history list
+  setopt hist_save_no_dups       # don't write a duplicate event to the history file
+  setopt hist_verify             # don't execute immediately upon history expansion
+  setopt inc_append_history      # write to the history file immediately, not when the shell exits
+  unsetopt share_history         # don't share history between all sessions
+
   SAVEHIST="${SAVEHIST:-10000}"
   HISTSIZE="${HISTSIZE:-10000}"
   if [[ -z $HISTFILE ]]; then
