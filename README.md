59f26f75d88ace0f0508af338f50bdb104e73ef2
mattmc3 <mattmc3@gmail.com>
2020-09-02 10:16:05 -0400

    Refactor for next version

--- README.md
+++ README.md
@@ -11,64 +11,35 @@
      src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Zebrafisch.jpg/512px-Zebrafisch.jpg">
 </a>
 
-A solid base ZSH configuration achieved with one small, simple include.
+A full featured, modern ZSH configuration packed into one simple include.
 
 Borrows some great ideas from [fish shell][fish-shell], but with a ZSH flair.
 
 ## What's included
 
-### Leverage amazing ZSH features using what already comes with ZSH
+### Leverage amazing ZSH features
 
-- Sets better ZSH default options
-- Sets good defaults for common environment variables
+- Sets the best [ZSH options][zsh-options] so you start off with a great ZSH configuration
+- Sets common environment variables
 - Adds a sane history configuration
 - Auto-sources any ZSH config files you put in `${ZDOTDIR:-$HOME}/.zshrc.d`
 - Lazy loads any ZSH function files you put in `${ZDOTDIR:-$HOME}/.zfunctions`
-- Adds great default keybindings
-- Sets the [XDG base directories][xdg-basedirs] if they haven't already been set
-
-### Extend ZSH features with other ZSH projects from around the web
-
-- Auto-sources any ZSH plugins you put in `${ZDOTDIR:-$HOME}/.zplugins`
-- Adds prompt themes to your `fpath` for any prompts in `${ZDOTDIR:-$HOME}/.zprompts`
+- Leverages the built in ZSH prompt functionality for themeing
+- Sets common keybindings
+- Uses the [XDG base directories][xdg-basedirs] spec so your `$HOME` remains uncluttered
+- Uses zstyle instead of  environment variables
 
 ### More information
 
-Zebrafish's features are covered in more detail on the
-[features.md](docs/features.md) page.
-
-## What's not included
-
-Zebrafish doesn't include anything you don't get out of the box with ZSH. That
-means that "plugins" and "prompts" from other git projects are not directly
-included in this project, though they are supported. Sometimes you are on a
-server where arbitrary ZSH code from the internet is not advisable, in which
-case Zebrafish will still work just fine as a standalone file - tiny and fast.
-
-There are a lot of great ZSH plugins out there, and I highly recommend grabbing
-a few to add even more features to your ZSH config. You can use a plugin manager
-like [zgen] if you think you want one, though `git` is simple and easy too.
-Zebrafish can be treated like a plugin itself and loaded from a plugin manager,
-or just a simple stand alone include from your .zshrc and then it can load
-plugins you pulled with `git` into `${ZDOTDIR:-$HOME}/.zplugins}`.
+Zebrafish's features are covered in more detail on the [features.md](docs/features.md) page.
 
 ## How is Zebrafish different?
 
 ### Compared to frameworks
 
-Projects like [oh-my-zsh], [zimfw] and [prezto] are neat. They do a ton of work
-to create a full featured ZSH environment, and make ZSH welcoming and exciting
-for new users. But these frameworks have downsides. They can be slow. They have
-a ton of files and configs and plugins. They are in charge of bootstrapping your
-whole ZSH configuration. They mix concepts of themes, aliases, functions, shell
-options, and plugins all into one giant soup of ZSH stuff. It's hard to know
-what it's all doing, and even harder to modify the parts you might not like.
+Projects like [oh-my-zsh], [zimfw] and [prezto] are neat. They do a ton of work to create a full featured ZSH environment, and make ZSH welcoming and exciting for new users. But these frameworks have downsides. They can be slow. They have a ton of files and configs and plugins. They are in charge of bootstrapping your whole ZSH configuration. They mix concepts of themes, aliases, functions, shell options, and plugins all into one giant soup of ZSH stuff. It's hard to know what it's all doing, and even harder to modify the parts you might not like.
 
-Zebrafish is different because it's not a ZSH framework that tries to do
-everything under the sun. Instead, it's just a set of sane defaults and handy
-tools that lets you easily build up the rest of you ZSH config from there. You
-can even use it with a ZSH framework if you prefer. Since Zebrafish is housed
-in a single file, you also can easily do things like:
+Zebrafish is different because it's not a ZSH framework that tries to do everything under the sun. Instead, it's just a set of sane defaults that lets you easily build up the rest of you ZSH config from there. You can even use it with a ZSH framework if you prefer. Since Zebrafish is housed in a single file, you also can easily do things like:
 
 - See everything Zebrafish does in one place
 - Turn off parts you don't like
@@ -84,18 +55,13 @@ in a single file, you also can easily do things like:
 - Share your ZSH configuration with your co-workers and friends easily without
   forcing them to commit to using a whole framework
 
-If you like [oh-my-zsh] or [prezto], you can keep using them with Zebrafish.
-Zebrafish is meant to be built upon. It just sets up your initial ZSH
-configuration in a way that makes sense so you don't have to do it all.
+If you like [oh-my-zsh] or [prezto], you can keep using them with Zebrafish. Zebrafish is meant to be built upon. It just sets up your initial ZSH configuration in a way that makes sense so you don't have to do it all.
 
-Just keep in mind that loading other plugins and frameworks _after_ Zebrafish
-means it's likely you overrode some of Zebrafish's settings.
+Just keep in mind that loading other plugins and frameworks _after_ Zebrafish means it's likely you overrode some of Zebrafish's settings.
 
 ### Compared to just using .zshrc
 
-There's no reason you can't put the contents of Zebrafish directly in your
-.zshrc. But, this project is great for folks that like a clean .zshrc file and
-want a reasonable base configuration to build their .zshrc from.
+There's no reason you can't put the contents of Zebrafish directly in your .zshrc. But, this project is great for folks that like a clean .zshrc file and want a reasonable base configuration to build their .zshrc from.
 
 ## Customization
 
@@ -108,35 +74,36 @@ in this project describes those options.
 
 Installation methods:
 
-- [zgen]: `zgen load mattmc3/zebrafish`
+- [zgen]: `zgen load zshzoo/zebrafish`
 - [oh-my-zsh]:
 
   ```shell
   ZSH_CUSTOM=${ZSH_CUSTOM:-$ZSH/custom}
-  git clone --depth 1 https://github.com/mattmc3/zebrafish.git $ZSH_CUSTOM/zebrafish
+  git clone --depth 1 https://github.com/zshzoo/zebrafish.git $ZSH_CUSTOM/zebrafish
   # now add zebrafish to your plugins variable in .zshrc
   ```
 
-- [antibody]: `antibody bundle mattmc3/zebrafish`
-- [antigen]: `antigen bundle mattmc3/zebrafish`
+- [antibody]: `antibody bundle zshzoo/zebrafish`
+- [antigen]: `antigen bundle zshzoo/zebrafish`
 - manually with curl:
 
   ```shell
-  curl -s -o ${ZDOTDIR:-$HOME}/.zebrafish.zsh https://raw.githubusercontent.com/mattmc3/zebrafish/master/zebrafish.plugin.zsh
+  curl -s -o ${ZDOTDIR:-$HOME}/.zebrafish.zsh https://raw.githubusercontent.com/zshzoo/zebrafish/master/zebrafish.plugin.zsh
   echo ". ${ZDOTDIR:-$HOME}/.zebrafish.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
   ```
 
-[antibody]:                      https://getantibody.github.io
-[antigen]:                       https://github.com/zsh-users/antigen
-[dotfiles]:                      https://dotfiles.github.io/
-[fast-syntax-highlighting]:      https://github.com/zdharma/fast-syntax-highlighting
-[fish-shell]:                    https://fishshell.com/
-[oh-my-zsh]:                     https://github.com/robbyrussell/oh-my-zsh
-[prezto]:                        https://github.com/sorin-ionescu/prezto
-[xdg-basedirs]:                  https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
-[zimfw]:                         https://github.com/zimfw/zimfw
-[zgen]:                          https://github.com/tarjoilija/zgen
-[zsh-autosuggestions]:           https://github.com/zsh-users/zsh-autosuggestions
-[zsh-completions]:               https://github.com/zsh-users/zsh-completions
+[antibody]:  https://getantibody.github.io
+[antigen]:  https://github.com/zsh-users/antigen
+[dotfiles]:  https://dotfiles.github.io/
+[fast-syntax-highlighting]:  https://github.com/zdharma/fast-syntax-highlighting
+[fish-shell]:  https://fishshell.com/
+[oh-my-zsh]:  https://github.com/ohmyzsh/ohmyzsh
+[prezto]:  https://github.com/sorin-ionescu/prezto
+[xdg-basedirs]:  https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+[zgen]:  https://github.com/tarjoilija/zgen
+[zimfw]:  https://github.com/zimfw/zimfw
+[zsh-autosuggestions]:  https://github.com/zsh-users/zsh-autosuggestions
+[zsh-completions]:  https://github.com/zsh-users/zsh-completions
 [zsh-history-substring-search]:  https://github.com/zsh-users/zsh-history-substring-search
-[zsh-prompt-theme]:              http://zsh.sourceforge.net/Doc/Release/User-Contributions.html#Prompt-Themes
+[zsh-options]:  http://zsh.sourceforge.net/Doc/Release/Options.html
+[zsh-prompt-theme]:  http://zsh.sourceforge.net/Doc/Release/User-Contributions.html#Prompt-Themes
--- zebrafish.plugin.zsh
+++ zebrafish.plugin.zsh
@@ -1,140 +1,191 @@
-# Copyright (c) 2019 mattmc3
+# Copyright (c) 2020 mattmc3
 # MIT license
-# zebrafish.plugin.zsh
-# version: 0.4.2
-# homepage: https://github.com/mattmc3/zebrafish
+# .zebrafish.zsh
+# version: 0.5.0-alpha
+# homepage: https://github.com/zshzoo/zebrafish
 #
-# A solid base zsh configuration achieved with only one small, simple include.
+# A full featured zsh base configuration in a simple include
 #
 # single file install:
-#   curl -s -o ${ZDOTDIR:-$HOME}/.zebrafish.zsh https://raw.githubusercontent.com/mattmc3/zebrafish/master/zebrafish.plugin.zsh
+#   curl -s -o ${ZDOTDIR:-$HOME}/.zebrafish.zsh https://raw.githubusercontent.com/zshzoo/zebrafish/master/.zebrafish.zsh
 #   echo '. ${ZDOTDIR:-$HOME}/.zebrafish.zsh' >> ${ZDOTDIR:-$HOME}/.zshrc
 #
-# install as plugin with zgen:
-#   zgen load mattmc3/zebrafish
+# To see the configuration options, run this after sourcing this file:
+# `zstyle -L ':zebrafish*'`
 
-#
-## Init
-# setup variables from zstyle settings
-_dotprefix="."
-if [[ -n $ZDOTDIR ]] && [[ $ZDOTDIR != $HOME ]]; then
-  # no reason to hide directories in $ZDOTDIR
-  _dotprefix=""
+
+# Init
+# ------------------------------------------------------------------------------
+_features=
+if [[ -z "$ZEBRAFISH_DIR" ]]; then
+  if [[ -n "$ZDOTDIR" ]]; then
+    ZEBRAFISH_DIR="$ZDOTDIR"
+  else
+    ZEBRAFISH_DIR="${XDG_CONFIG_DIR:-$HOME/.config}/zebrafish"
+  fi
+fi
+[[ -d "$ZEBRAFISH_DIR" ]] || mkdir -p "$ZEBRAFISH_DIR"
+
+_zshrcd_path=
+_zfuncs_path=
+_plugins_path=
+_zf_init() {
+  local d zfuncs_dirname zshrcd_dirname
+
+  # if not defined, define the defaults
+  if zstyle -T ':zebrafish:features' 'enable'; then
+    zstyle ':zebrafish:features' 'enable' \
+      'completions' \
+      'history' \
+      'environment' \
+      'key-bindings' \
+      'zshopts' \
+      'plugins' \
+      'prompt' \
+      'xdg-base-dirs' \
+      'zfunctions' \
+      'zshrc.d'
+  fi
+
+  # set zebrafish path
+  if zstyle -T ':zebrafish:core' 'path'; then
+    if [[ -n "$ZDOTDIR" ]]; then
+      zstyle ':zebrafish:core' 'path' "$ZDOTDIR"
+    else
+      zstyle ':zebrafish:core' 'path' "${XDG_CONFIG_DIR:-$HOME/.config}/zebrafish"
+    fi
+  fi
+  zstyle -g _zebrafish_path ':zebrafish:core' 'path'
+
+  # set defaults
+  if zstyle -T ':zebrafish:features:plugins' 'dirname'; then
+    zstyle ':zebrafish:features:plugins' 'dirname' "plugins"
+  fi
+  if zstyle -T ':zebrafish:features:zshrc.d' 'dirname'; then
+    zstyle ':zebrafish:features:zshrc.d' 'dirname' "zshrc.d"
+  fi
+  if zstyle -T ':zebrafish:features:zfunctions' 'dirname'; then
+    zstyle ':zebrafish:features:zfunctions' 'dirname' "zfunctions"
+  fi
+
+  # get variables for all the necessary feature dirs
+  zstyle -g zfuncs_dirname ':zebrafish:features:zfunctions' 'dirname'
+  _zfuncs_path="${_zebrafish_path}/${zfuncs_dirname}"
+
+  zstyle -g plugins_dirname ':zebrafish:features:plugins' 'dirname'
+  _plugins_path="${_zebrafish_path}/${plugins_dirname}"
+  export PLUGZ_DIR="${_plugins_path}"
+
+  # get the list of features
+  zstyle -g _features ':zebrafish:features' 'enable'
+}
+_zf_init
+
+
+# ZSH Options
+# ------------------------------------------------------------------------------
+if (($_features[(Ie)zshopts])); then
+  # http://zsh.sourceforge.net/Doc/Release/Options.html
+
+  # Changing Directories
+  # http://zsh.sourceforge.net/Doc/Release/Options.html#Changing-Directories
+  setopt auto_cd                 # if a command isn't valid, but is a directory, cd to that dir
+  setopt auto_pushd              # make cd push the old directory onto the directory stack
+  setopt pushd_ignore_dups       # don’t push multiple copies of the same directory onto the directory stack
+  setopt pushd_minus             # exchanges the meanings of ‘+’ and ‘-’ when specifying a directory in the stack
+
+  # Completions
+  # http://zsh.sourceforge.net/Doc/Release/Options.html#Completion-2
+  setopt always_to_end           # move cursor to the end of a completed word
+  setopt auto_list               # automatically list choices on ambiguous completion
+  setopt auto_menu               # show completion menu on a successive tab press
+  setopt auto_param_slash        # if completed parameter is a directory, add a trailing slash
+  setopt complete_in_word        # complete from both ends of a word
+  unsetopt menu_complete         # don't autoselect the first completion entry
+
+  # Expansion and Globbing
+  # http://zsh.sourceforge.net/Doc/Release/Options.html#Expansion-and-Globbing
+  setopt extended_glob           # use more awesome globbing features
+  setopt glob_dots               # include dotfiles when globbing
+
+  # History
+  # set in history feature
+
+  # Initialization
+  # http://zsh.sourceforge.net/Doc/Release/Options.html#Initialisation
+
+  # Input/Output
+  # http://zsh.sourceforge.net/Doc/Release/Options.html#Input_002fOutput
+  unsetopt clobber               # must use >| to truncate existing files
+  unsetopt correct               # don't try to correct the spelling of commands
+  unsetopt correct_all           # don't try to correct the spelling of all arguments in a line
+  unsetopt flow_control          # disable start/stop characters in shell editor
+  setopt interactive_comments    # enable comments in interactive shell
+  unsetopt mail_warning          # don't print a warning message if a mail file has been accessed
+  setopt path_dirs               # perform path search even on command names with slashes
+  setopt rc_quotes               # allow 'Henry''s Garage' instead of 'Henry'\''s Garage'
+  unsetopt rm_star_silent        # ask for confirmation for `rm *' or `rm path/*'
+
+  # Job Control
+  # http://zsh.sourceforge.net/Doc/Release/Options.html#Job-Control
+  setopt auto_resume            # attempt to resume existing job before creating a new process
+  unsetopt bg_nice              # don't run all background jobs at a lower priority
+  unsetopt check_jobs           # don't report on jobs when shell exit
+  unsetopt hup                  # don't kill jobs on shell exit
+  setopt long_list_jobs         # list jobs in the long format by default
+  setopt notify                 # report status of background jobs immediately
+
+  # Prompting
+  # http://zsh.sourceforge.net/Doc/Release/Options.html#Prompting
+  setopt prompt_subst           # expand parameters in prompt variables
+
+  # Scripts and Functions
+  # http://zsh.sourceforge.net/Doc/Release/Options.html#Scripts-and-Functions
+
+  # Shell Emulation
+  # http://zsh.sourceforge.net/Doc/Release/Options.html#Shell-Emulation
+
+  # Shell State
+  # http://zsh.sourceforge.net/Doc/Release/Options.html#Shell-State
+
+  # Zle
+  # http://zsh.sourceforge.net/Doc/Release/Options.html#Zle
+  unsetopt beep                 # be quiet!
+  setopt combining_chars        # combine zero-length punctuation characters (accents) with the base character
+  setopt emacs                  # use emacs keybindings in the shell
 fi
 
-# if not defined, define the defaults
-if zstyle -T ':zebrafish:features' 'enable'; then
-  zstyle ':zebrafish:features' 'enable' 'compinit' 'history' 'environment' 'key-bindings' 'xdg' 'zfunctions' 'zshrc.d'
-fi
-if zstyle -T ':zebrafish:zshrc.d' 'path'; then
-  zstyle ':zebrafish:zshrc.d' 'path' "${ZDOTDIR:-$HOME}"/${_dotprefix}zshrc.d
-fi
-if zstyle -T ':zebrafish:zfunctions' 'path'; then
-  zstyle ':zebrafish:zfunctions' 'path' "${ZDOTDIR:-$HOME}"/${_dotprefix}zfunctions
-fi
 
-# load script variables from zstyle settings
-zstyle -g _features ':zebrafish:features' 'enable'
-zstyle -g _zshrcd_dir ':zebrafish:zshrc.d' 'path'
-zstyle -g _zfunctions_dir ':zebrafish:zfunctions' 'path'
-
-#
-## XDG
-if (($_features[(Ie)xdg])); then
+# Feature: XDG
+# ------------------------------------------------------------------------------
+_zf_xdg() {
   export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
   export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
   export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
   export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-$HOME/.xdg}"
+}
+if (($_features[(Ie)xdg-base-dirs])); then
+  _zf_xdg
 fi
 
-#
-## ZSH Options
-# http://zsh.sourceforge.net/Doc/Release/Options.html
 
-# Changing Directories
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Changing-Directories
-setopt auto_cd                 # if a command isn't valid, but is a directory, cd to that dir
-setopt auto_pushd              # make cd push the old directory onto the directory stack
-setopt pushd_ignore_dups       # don’t push multiple copies of the same directory onto the directory stack
-setopt pushd_minus             # exchanges the meanings of ‘+’ and ‘-’ when specifying a directory in the stack
-
-# Completions
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Completion-2
-setopt always_to_end           # move cursor to the end of a completed word
-setopt auto_list               # automatically list choices on ambiguous completion
-setopt auto_menu               # show completion menu on a successive tab press
-setopt auto_param_slash        # if completed parameter is a directory, add a trailing slash
-setopt complete_in_word        # complete from both ends of a word
-unsetopt menu_complete         # don't autoselect the first completion entry
-
-# Expansion and Globbing
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Expansion-and-Globbing
-setopt extended_glob           # use more awesome globbing features
-setopt glob_dots               # include dotfiles when globbing
-
-# History
-# set in history feature
-
-# Initialization
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Initialisation
-
-# Input/Output
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Input_002fOutput
-unsetopt clobber               # must use >| to truncate existing files
-unsetopt correct               # don't try to correct the spelling of commands
-unsetopt correct_all           # don't try to correct the spelling of all arguments in a line
-unsetopt flow_control          # disable start/stop characters in shell editor
-setopt interactive_comments    # enable comments in interactive shell
-unsetopt mail_warning          # don't print a warning message if a mail file has been accessed
-setopt path_dirs               # perform path search even on command names with slashes
-setopt rc_quotes               # allow 'Henry''s Garage' instead of 'Henry'\''s Garage'
-unsetopt rm_star_silent        # ask for confirmation for `rm *' or `rm path/*'
-
-# Job Control
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Job-Control
-setopt auto_resume            # attempt to resume existing job before creating a new process
-unsetopt bg_nice              # don't run all background jobs at a lower priority
-unsetopt check_jobs           # don't report on jobs when shell exit
-unsetopt hup                  # don't kill jobs on shell exit
-setopt long_list_jobs         # list jobs in the long format by default
-setopt notify                 # report status of background jobs immediately
-
-# Prompting
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Prompting
-setopt prompt_subst           # expand parameters in prompt variables
-
-# Scripts and Functions
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Scripts-and-Functions
-
-# Shell Emulation
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Shell-Emulation
-
-# Shell State
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Shell-State
-
-# Zle
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Zle
-unsetopt beep                 # be quiet!
-setopt combining_chars        # combine zero-length punctuation characters (accents) with the base character
-setopt emacs                  # use emacs keybindings in the shell
-
-#
-## Environment
-if (($_features[(Ie)environment])); then
-  export LANG="${LANG:-en_US.UTF-8}"
-  export LANGUAGE="${LANGUAGE:-en}"
-  export LC_ALL="${LC_ALL:-en_US.UTF-8}"
-
+# Feature: Environment
+# ------------------------------------------------------------------------------
+_zf_env() {
   export CLICOLOR="${CLICOLOR:-1}"
   export LSCOLORS="${LSCOLORS:-ExfxcxdxbxGxDxabagacad}"
 
   export PAGER="${PAGER:-less}"
   export EDITOR="${EDITOR:-vim}"
   export VISUAL="${VISUAL:-vim}"
+}
+if (($_features[(Ie)environment])); then
+  _zf_env
 fi
 
-#
-## History
+
+# Feature: History
+# ------------------------------------------------------------------------------
 if (($_features[(Ie)history])); then
   # http://zsh.sourceforge.net/Doc/Release/Options.html#History
   setopt append_history          # append to history file
@@ -152,26 +203,30 @@ if (($_features[(Ie)history])); then
   setopt inc_append_history      # write to the history file immediately, not when the shell exits
   unsetopt share_history         # don't share history between all sessions
 
-  SAVEHIST=10000
-  HISTSIZE=10000
-  if [[ -z $HISTFILE ]]; then
-    if [[ -d $XDG_DATA_HOME ]]; then
-      HISTFILE="$XDG_DATA_HOME/zsh/zhistory"
-      [[ -d "$XDG_DATA_HOME"/zsh ]] || mkdir -p "$XDG_DATA_HOME"/zsh
-      [[ -f "${ZDOTDIR:-$HOME}/.zhistory" ]] || ln -sf "$HISTFILE" "${ZDOTDIR:-$HOME}/.zhistory"
-    else
-      HISTFILE="${ZDOTDIR:-$HOME}/.zhistory"
+  if [[ -z "$SAVEHIST" ]] || [[ $SAVEHIST -le 1000 ]]; then
+    SAVEHIST=5000
+  fi
+  if [[ -z "$HISTSIZE" ]] || [[ $HISTSIZE -le 2000 ]]; then
+    HISTSIZE=10000
+  fi
+  if [[ -d $XDG_DATA_HOME ]]; then
+    HISTFILE="$XDG_DATA_HOME/zsh/history"
+    [[ -d "$XDG_DATA_HOME"/zsh ]] || mkdir -p "$XDG_DATA_HOME"/zsh
+    if [[ ! -f "${ZDOTDIR:-$HOME}/.zsh_history" ]]; then
+      ln -sf "$HISTFILE" "${ZDOTDIR:-$HOME}/.zsh_history"
     fi
+  else
+    HISTFILE="${ZDOTDIR:-$HOME}/.zsh_history"
   fi
 fi
 
-#
-## Key-bindings
-# TODO: verify this is complete and correct and comment it better
-# https://github.com/fish-shell/fish-shell/blob/master/share/functions/fish_default_key_bindings.fish
-# http://fishshell.com/docs/current/index.html#editor
-# https://github.com/changs/slimzsh/blob/master/keys.zsh
-if (($_features[(Ie)key-bindings])); then
+
+# Feature: Key Bindings
+# ------------------------------------------------------------------------------
+_zf_key-bindings() {
+  # https://github.com/fish-shell/fish-shell/blob/master/share/functions/fish_default_key_bindings.fish
+  # http://fishshell.com/docs/current/index.html#editor
+  # https://github.com/changs/slimzsh/blob/master/keys.zsh
   export KEYTIMEOUT=${KEYTIMEOUT:-1}  # remove lag
   bindkey -e
   bindkey '\ew' kill-region
@@ -203,11 +258,84 @@ if (($_features[(Ie)key-bindings])); then
   bindkey '^I' complete-word
   # Fix weird sequence that rxvt produces
   bindkey -s '^[[Z' '\t'
+}
+if (($_features[(Ie)key-bindings])); then
+  _zf_key-bindings
 fi
 
-#
-## Completions
-_zcompinit() {
+
+# Feature: Plugins
+# ------------------------------------------------------------------------------
+_zf_plugins() {
+  if [[ ! -d "$PLUGZ_DIR" ]]; then
+    git clone -b develop https://github.com/zshzoo/plugz.git "$PLUGZ_DIR"
+  fi
+  PLUGZ_RESET_ON_CHANGE=(${ZDOTDIR:-$HOME}/.zshrc ${ZDOTDIR:-$HOME}/.zplugins)
+  PLUGZ_AUTOLOAD_COMPINIT=false
+
+  if [[ ! -f ${ZDOTDIR:-$HOME}/.zplugins ]]; then
+    cat <<EOF > ${ZDOTDIR:-$HOME}/.zplugins
+zsh-users/zsh-autosuggestions
+zsh-users/zsh-completions
+zsh-users/zsh-history-substring-search
+zsh-users/zsh-syntax-highlighting
+EOF
+  fi
+
+  # load plugins
+  source "$PLUGZ_DIR/plugz.zsh"
+  if ! plugz saved; then
+    # use zplugins file for other plugins if it exists
+    if [[ -f ${ZDOTDIR:-$HOME}/.zplugins ]]; then
+      plugz loadall < ${ZDOTDIR:-$HOME}/.zplugins
+    fi
+
+    # save plugz plugins into init.zsh
+    plugz save
+  fi
+}
+if (($_features[(Ie)plugins])); then
+  _zf_plugins
+fi
+
+
+# Feature: zfunctions
+# ------------------------------------------------------------------------------
+# auto load any function in the functions location
+_zf_zfuncs() {
+  local zfunc
+  [[ -d "$_zfuncs_path" ]] || mkdir -p "$_zfuncs_path"
+  fpath=("$_zfuncs_path" $fpath)
+  for zfunc in "$_zfuncs_path"/*(N); do
+    autoload -U "$zfunc"
+  done
+}
+if (($_features[(Ie)zfunctions])); then
+  _zf_zfuncs
+fi
+
+
+# Feature: zshrc.d
+# ------------------------------------------------------------------------------
+_zf_zshrcd() {
+  local zfile zshrcd_dirname zshrcd_path
+  zstyle -g zshrcd_dirname ':zebrafish:features:zshrc.d' 'dirname'
+  zshrcd_path="${_zebrafish_path}/${zshrcd_dirname}"
+  [[ -d "$_zshrcd_path" ]] || mkdir -p "$_zshrcd_path"
+  for zfile in "$_zshrcd_path"/*.{sh,zsh}(N); do
+    # ignore files that begin with a tilde
+    case $zfile:t in ~*) continue;; esac
+    source "$zfile"
+  done
+}
+if (($_features[(Ie)zshrc.d])); then
+  _zf_zshrcd
+fi
+
+
+# Feature: Completions
+# ------------------------------------------------------------------------------
+_zf_completions() {
   # https://github.com/sorin-ionescu/prezto/blob/master/modules/completion/init.zsh#L31-L41
   # http://zsh.sourceforge.net/Doc/Release/Completion-System.html#Use-of-compinit
   # https://gist.github.com/ctechols/ca1035271ad134841284#gistcomment-2894219
@@ -225,35 +353,34 @@ _zcompinit() {
     { [[ ! -f "$zcdc" || "$zcd" -nt "$zcdc" ]] && rm -f "$zcdc" && zcompile "$zcd" } &!
   fi
 }
-if (($_features[(Ie)compinit])); then
-  _zcompinit
+if (($_features[(Ie)completions])); then
+  _zf_completions
 fi
 
-#
-## .zfunctions
-# auto load any function in the functions location
-if (($_features[(Ie)zfunctions])) && [[ -d "$_zfunctions_dir" ]]; then
-  fpath=("$_zfunctions_dir" $fpath)
-  for _zfunc in "$_zfunctions_dir"/*(N); do
-    autoload -U "$_zfunc"
-  done
-  unset _zfunc
+
+# Feature: Prompt
+# ------------------------------------------------------------------------------
+_zf_prompt() {
+  autoload -U promptinit
+  promptinit
+}
+if (($_features[(Ie)prompt])); then
+  _zf_prompt
 fi
 
-#
-## .zshrc.d
-if (($_features[(Ie)zshrc.d])) && [[ -d "$_zshrcd_dir" ]]; then
-  for _zfile in "$_zshrcd_dir"/*.{sh,zsh}(N); do
-    # ignore files that begin with a tilde
-    case $_zfile:t in ~*) continue;; esac
-    source "$_zfile"
-  done
-  unset _zfile
-fi
 
-#
-## Cleanup
-unset _dotprefix
+# Cleanup
+# ------------------------------------------------------------------------------
 unset _features
-unset _zfunctions_dir
-unset _zshrcd_dir
+unset _zebrafish_path
+unset _zshrcd_path
+unset _zfuncs_path
+unfunction _zf_init
+unfunction _zf_xdg
+unfunction _zf_env
+unfunction _zf_key-bindings
+unfunction _zf_completions
+unfunction _zf_plugins
+unfunction _zf_zfuncs
+unfunction _zf_zshrcd
+unfunction _zf_prompt
